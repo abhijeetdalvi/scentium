5e71cadacc910df2512c3ddeef128b143d5e6033
Abhijeet Dalvi <abhijeetvdalvi@.com>
2022-08-15 11:04:54 +0200

    contact route and hbs not working

---
+++ routes/contact.routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+const isLoggedOut = require("../middleware/isLoggedOut");
+
+/* GET contact page */
+router.get("/contact", (req, res) => {
+  res.render("/views/contact");
+});
+
+router.post("/contact", isLoggedOut, (req, res) => {
+  const { name, email, enquiry } = req.body;
+});
+
+module.exports = router;
